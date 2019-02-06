6b4497d202cb1e4b7826670ac260f4ae4f5276e0
Emily Barnes <barnes_emily@berkeley.edu>
2019-02-06 14:09:22 -0800

    first commit, making blob table processor

---
+++ blobtable_processing.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(dplyr)
+
+
+bt1 <- read.csv(file = "C:\\Users\\GoldsteinGroup\\Desktop\\Research\\GoAmazon_filters\\blob_tables\\GCxGC_20181128_0739.h5_img01_Blob_Table.csv")
+
+make_file_name <- function(date_string) {
+  file_start <- "C:\\Users\\GoldsteinGroup\\Desktop\\Research\\GoAmazon_filters\\blob_tables\\GCxGC_"
+  file_end <- 
+  
+}
