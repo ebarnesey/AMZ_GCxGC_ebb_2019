3a6adb866b5c70e2856eec2c985bc90d406ab09a
Emily Barnes <barnes_emily@berkeley.edu>
2019-02-06 16:28:19 -0800

    did a LOT of work on this

--- blobtable_processing.R
+++ blobtable_processing.R
@@ -6,6 +6,41 @@ bt1 <- read.csv(file = "C:\\Users\\GoldsteinGroup\\Desktop\\Research\\GoAmazon_f
 
 make_file_name <- function(date_string) {
   file_start <- "C:\\Users\\GoldsteinGroup\\Desktop\\Research\\GoAmazon_filters\\blob_tables\\GCxGC_"
-  file_end <- 
+  file_end <- ".h5_img01_Blob_Table.csv"
+  full_name <- paste(file_start,date_string,file_end, sep = "")
+  return(full_name)
+  
+}
+
+make_file_name("20181128_0739")
+
+read_bt_files <- function(date_string) {
+  
+  fi_name <- make_file_name(date_string)
+  run_date <- as.Date(as.character(gsub("_", "", date_string)), format = "%Y%m%d")
+  temp_bt <<- read.csv(file = fi_name)
+  temp_bt$RunDate = run_date
+  temp_bt <<- temp_bt
+  return(temp_bt)
+}
+
+read_bt_files("20181128_0739")
+
+summary_table <- read.csv("C:\\Users\\GoldsteinGroup\\Desktop\\Research\\GoAmazon_filters\\blob_tables\\Blob_table_summary.csv")
+
+make_massive_table <- function(summary_table){
+ 
+  M <<- read_bt_files(as.character(summary_table$File_num[1]))
+  M$Category = as.character(summary_table$Category[1])
+  M$Filter_num = as.integer(summary_table$Filter_num[1])
+  M$Filter_punches = as.integer(summary_table$Filter_punches[1])
+  M$IOP = as.integer(summary_table$IOP[1])
+  # need to sort out date conversion later this is a pain
+  #M$AMZ_date = as.Date(as.character(summary_table$AMZ_date[1]), format = '%m/%d/%Y %h:%m')
+  
+
+  for(i in 2:length(summary_table$File_num)){
+    
+  }
   
 }
