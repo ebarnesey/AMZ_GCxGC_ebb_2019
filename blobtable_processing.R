bfef950511ce806e37d33c4d68f731bfa877d849
Emily Barnes <barnes_emily@berkeley.edu>
2019-02-11 17:18:33 -0800

    fixed the loading problem

--- blobtable_processing.R
+++ blobtable_processing.R
@@ -45,7 +45,11 @@ make_massive_table <- function(summary_table){
   # need to sort out date conversion later this is a pain
   #M$AMZ_date = as.Date(as.character(summary_table$AMZ_date[1]), format = '%m/%d/%Y %h:%m')
   M$AMZ_date <- as.character(summary_table$AMZ_date[1])
-  M$total_loading <- sum(M$Volume)
+  M_tempvol <- M %>% 
+    filter(Library.Name != "amzi0503_b") %>% 
+    filter(Library.Name != "fb_1130_1618")
+  mtempvol <- sum(M_tempvol$Volume)
+  M$Loading <- mtempvol
   
 
   for(i in 2:length(summary_table$File_num)){
@@ -57,7 +61,12 @@ make_massive_table <- function(summary_table){
     # need to sort out date conversion later this is a pain
     #M$AMZ_date = as.Date(as.character(summary_table$AMZ_date[1]), format = '%m/%d/%Y %h:%m')
     t$AMZ_date <- as.character(summary_table$AMZ_date[i])
-    t$total_loading <- sum(t$Volume)
+    t_tempvol <- t %>% 
+      filter(Library.Name != "amzi0503_b") %>% 
+      filter(Library.Name != "fb_1130_1618")
+    tempvol <- sum(t_tempvol$Volume)
+    t$Loading <- tempvol
+      
     Mnew <- rbind(M, t)
     M <- Mnew
     print(i)
@@ -77,4 +86,59 @@ ggplot(M_t_IS_2punch, aes(x = Compound.Name, y = Volume))+
 
 ggplot(M_t_IS_2punch, aes(x = total_loading, y = Volume, color = Compound.Name))+
   geom_line()
+
+# Determine_loading <- function(Blobtable_a, new_btname) {
+#   temp <- Blobtable_a %>% 
+#     filter(Library != "amzi0503_b") %>% 
+#     filter(Library != "fb_") %>% 
+#     
+#   }
+
+
+Only_matches <- function(raw_bt, bt_onlymatch, min_match_factor, min_vol) {
+  onlymatch_bt_a <- raw_bt %>%
+    filter(Volume > min_vol, Library.Match.Factor > min_match_factor) %>% 
+    arrange(desc(Library.Match.Factor)) %>% 
+    arrange(desc(Compound.Name)) %>% 
+    arrange(desc(RunDate)) %>% 
+    distinct(Compound.Name, RunDate, .keep_all = TRUE)
   
+  assign(bt_onlymatch, onlymatch_bt_a, envir = globalenv())
+  return(bt_onlymatch)
+}
+
+bt_402 <- M_t %>% 
+  filter(Filter_num == 402)
+
+Only_matches(bt_402, "bt_402_onlymatch", 750, 150)
+
+bt_400 <- M_t %>% 
+  filter(Filter_num == 400)
+
+Only_matches(bt_400, "bt_400_onlymatch", 750, 150)
+
+
+# min_vol <- 150
+# min_match_factor <- 750
+# 
+# tt <- bt_402 %>%
+#   filter(Volume > min_vol, Library.Match.Factor > min_match_factor) %>% 
+#   arrange(desc(Library.Match.Factor)) %>% 
+#   arrange(desc(Compound.Name)) %>% 
+#   arrange(desc(RunDate)) %>% 
+#   distinct(Compound.Name, .keep_all = TRUE)
+# 
+# mtcars
+# 
+# subset_high_hp <- function(full_table, name_only) {
+#   only_highHP_a <- full_table %>% 
+#     filter(hp > 200)
+#   
+#   assign(name_only, only_highHP_a, envir = globalenv())
+#   
+# }
+# 
+# subset_high_hp(mtcars, "mtcars_highhp")
+# 
+# 
+#   
